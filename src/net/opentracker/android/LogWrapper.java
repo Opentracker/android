17bbff0253647bcd07b2bf51dd9246b55ced1d1e
Opentracker <development@opentracker.net>
2015-10-02 10:13:54 +0200

    Update LogWrapper.java
    Added licence

--- src/net/opentracker/android/LogWrapper.java
+++ src/net/opentracker/android/LogWrapper.java
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (C) 2011 Opentracker.net 
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  The full license is located at the root of this distribution
+ *  in the LICENSE file.
+ *
+ *  Please report bugs to support@opentracker.net
+ *
+ */
 package net.opentracker.android;
 
 import android.util.Log;
